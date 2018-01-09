b2635131573046b7ed36d7cc31a44bb605468ad3
Justin Ahn <jeeyeop@justins-mbp.frontierlocal.net>
2018-01-09 10:38:41 -0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Justin Ahn on 1/7/18.
+//  Copyright © 2018 Justin Ahn. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
